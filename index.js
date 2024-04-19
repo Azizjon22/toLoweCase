45f3dbe7f36e0ea03b1cbb8119ebb56f5acee24d
Azizjon22 <yakubjonovazizjon@gmail.ru>
2024-04-19 19:40:35 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+const str = (str1) =>{
+    if(str1 === str1.toLowerCase()){
+        return str1.toUpperCase()
+    }else{
+        return str1.toLowerCase()
+    }
+}
+
+console.log(str('AZIZ'));
